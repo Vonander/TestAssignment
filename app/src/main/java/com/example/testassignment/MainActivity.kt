69328365de45e02c78768f8a53d0352fa66c956e
Vonander <johan.fornander@outlook.com>
2022-06-07 11:08:26 +0200

    some commit message
    Change-Id: Id792564dfe54070b9bf688ce8cbcc995a04566a9

--- app/src/main/java/com/example/testassignment/MainActivity.kt
+++ app/src/main/java/com/example/testassignment/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : ComponentActivity() {
         *
         * */
 
-        //testing yet again
+        //Det blev fel, typ
 
         setContent {
             TestAssignmentTheme {
